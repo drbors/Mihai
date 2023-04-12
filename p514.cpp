c7fb4094337d0c2b43e91e157823167b2ee1c83b
Dragos B <drbors@yahoo.com>
2023-04-13 01:57:55 +0300

    init

---
+++ p514.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int n, arr[100], b;
+void sortArray();
+void FiltreazaNrPrime();
+void CitesteDatele();
+void AfiseazaDatele();
+
+//TODO: scrie enuntul problemei si care e sursa
+//Se dă un vector cu n elemente, numere naturale. Afișați în ordine descrescătoare 
+//  valorile din vector care sunt prime cu ultimul element al vectorului.
+int main()
+{
+    //TODO
+    //CitesteDatele();
+    //FiltreazaNrPrime();
+    //sortArray();
+    //AfiseazaDatele();
+
+    cout << "ok, done!";
+
+    return 0;
+}
+
+void  sortArray()
+{
+
+}
+
+void CitesteDatele()
+{
+    ifstream fin("p514_1.in");
+    fin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        fin >> arr[i];
+    }
+    fin.close();
+}
+
+void AfiseazaDatele()
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+void FiltreazaNrPrime()
+{
+
+}
